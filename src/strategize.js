e868292404dbc7871ff7f2b4975fd32063b2dff4
James Leigh <james@leighnet.ca>
2018-05-26 08:14:14 -0400

    If strategy from scratch is the same as given, just return given

--- src/strategize.js
+++ src/strategize.js
@@ -173,6 +173,7 @@ function strategizeLegs(bestsignals, prng, parser, termAt, started, options, sig
             var cost = getStrategyCost(strategy.expr, options);
             var better = reset[strategy_var];
             if (latestScore - cost < better.score - better.cost) return reset;
+            if (latestScore - cost == better.score - better.cost) return signals;
             logger.log("Strategize", options.label || '\b', "from scratch was not good enough", better.variables[strategy_var], better.score);
             return some(scores, signals);
         });
