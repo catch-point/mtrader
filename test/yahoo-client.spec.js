afa7b7fb7bea24c98292eddb7787b444d8c95d09
James Leigh <james@leighnet.ca>
2019-10-29 21:01:17 -0400

    Add more Yahoo! tests

--- test/yahoo-client.spec.js
+++ test/yahoo-client.spec.js
@@ -57,4 +57,43 @@ describe("yahoo-client", function() {
             {symbol: 'C', name: name => name.toLowerCase().indexOf("citigroup") === 0}
         )));
     });
+    it("should find dividend for SPY", function() {
+        return client.dividend('SPY', '2019-01-01', 'America/New_York')
+          .then(data => data.filter(d => d.Date < '2020'))
+          .then(data => _.sortBy(data, 'Date'))
+          .then(data => data.slice(data.length-3,data.length))
+          .should.eventually.be.like([
+            { Date: '2019-03-15', Dividends: '1.233' },
+            { Date: '2019-06-21', Dividends: '1.432' },
+            { Date: '2019-09-20', Dividends: '1.384' }
+        ]);
+    });
+    it("should find dividends for XLK", function() {
+        return client.dividend('XLK', '2010-01-01', 'America/New_York')
+          .then(data => data.filter(d => d.Date < '2015'))
+          .then(data => _.sortBy(data, 'Date'))
+          .should.eventually.be.like([
+            { Date: '2010-03-19', Dividends: '0.073' },
+            { Date: '2010-06-18', Dividends: '0.079' },
+            { Date: '2010-09-17', Dividends: '0.081' },
+            { Date: '2010-12-17', Dividends: '0.09' },
+            { Date: '2011-03-18', Dividends: '0.084' },
+            { Date: '2011-06-17', Dividends: '0.098' },
+            { Date: '2011-09-16', Dividends: '0.094' },
+            { Date: '2011-12-16', Dividends: '0.109' },
+            { Date: '2012-03-16', Dividends: '0.096' },
+            { Date: '2012-06-15', Dividends: '0.109' },
+            { Date: '2012-09-21', Dividends: '0.13' },
+            { Date: '2012-12-21', Dividends: '0.168' },
+            { Date: '2013-03-15', Dividends: '0.124' },
+            { Date: '2013-06-21', Dividends: '0.155' },
+            { Date: '2013-09-20', Dividends: '0.158' },
+            { Date: '2013-12-20', Dividends: '0.171' },
+            { Date: '2014-03-21', Dividends: '0.157' },
+            { Date: '2014-06-20', Dividends: '0.176' },
+            { Date: '2014-09-19', Dividends: '0.174' },
+            { Date: '2014-12-19', Dividends: '0.216' }
+
+        ]);
+    });
 });
